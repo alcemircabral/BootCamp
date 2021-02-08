c919534c382c8faa1b8f13078871dc116a930300
Alcemir Cabral <alcemircg@gmail.com>
2021-02-08 06:43:52 -0300

    Aula 1 JavaScript

---
+++ JavaScript/Aula1/main.js
@@ -0,0 +1,11 @@
+let nome = "Alcemir Cabral";
+let idade = 37;
+let idade2 = 10;
+let frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos." );
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+//alert(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
