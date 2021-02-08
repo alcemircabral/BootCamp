35004a02f897642ac2b720d7a336832ea73b1430
Alcemir Cabral <alcemircg@gmail.com>
2021-02-08 19:39:48 -0300

    Atualização JS

---
+++ JavaScript/Aula2/main.js
@@ -0,0 +1,21 @@
+//---------------ARRAY------------------------
+//let lista = ["maçã", "pêra","laranja"];
+//lista.pop();
+//lista.push("uva");
+//console.log(lista.reverse());
+//console.log(lista[0]);
+//console.log(lista.toString()[0]);
+//console.log(lista.join(" | "));
+
+//--------------DICIONARIO---------------------
+/*
+let fruta = {nome: "maçã", cor: "vermelha"} //Objeto JSON
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//-------------LISTA DE DICIONARIO--------------
+
+let frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}];
+console.log(frutas);
+alert(frutas[0].nome);
